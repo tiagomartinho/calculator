067a0112a910dd0d8c4248adfa7d8ece815e1e6e
tiagomartinho <tiago.martinho@xpeppers.com>
2015-05-01 17:05:38 +0200

    refactor operation tests replace all calculator brain calls with optional chaining

--- CalculatorTests/CalculatorOperationTests.swift
+++ CalculatorTests/CalculatorOperationTests.swift
@@ -10,80 +10,80 @@ class CalculatorOperationTests: XCTestCase {
     }
     
     func testOneOperand(){
-        calculatorBrain!.pushOperand(5)
+        calculatorBrain?.pushOperand(5)
         calculatorBrain?.assertItEvaluatesTo(5.0)
     }
 
     func testOneOperandWithPosUnaryOperation(){
-        calculatorBrain!.pushOperand(2)
-        calculatorBrain!.pushOperation("^2")
+        calculatorBrain?.pushOperand(2)
+        calculatorBrain?.pushOperation("^2")
         calculatorBrain?.assertItEvaluatesTo(4.0)
     }
     
     func testOneOperandWithPreUnaryOperation(){
-        calculatorBrain!.pushOperation("√")
-        calculatorBrain!.pushOperand(4)
+        calculatorBrain?.pushOperation("√")
+        calculatorBrain?.pushOperand(4)
         calculatorBrain?.assertItEvaluatesTo(2.0)
     }
     
     func testTwoOperandsWithBinaryOperation(){
-        calculatorBrain!.pushOperand(1)
-        calculatorBrain!.pushOperation("-")
-        calculatorBrain!.pushOperand(2)
+        calculatorBrain?.pushOperand(1)
+        calculatorBrain?.pushOperation("-")
+        calculatorBrain?.pushOperand(2)
         calculatorBrain?.assertItEvaluatesTo(-1.0)
     }
     
     func testMultiplicationOfTwoOperandsPlusSum(){
-        calculatorBrain!.pushOperand(1)
-        calculatorBrain!.pushOperation("*")
-        calculatorBrain!.pushOperand(2)
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperand(4)
+        calculatorBrain?.pushOperand(1)
+        calculatorBrain?.pushOperation("*")
+        calculatorBrain?.pushOperand(2)
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperand(4)
         calculatorBrain?.assertItEvaluatesTo(6.0)
     }
     
     func testPreUnaryFollowByBinaryOperation(){
-        calculatorBrain!.pushOperation("√")
-        calculatorBrain!.pushOperand(4)
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperand(4)
-        XCTAssertEqual(6.0, calculatorBrain!.evaluate()!)
+        calculatorBrain?.pushOperation("√")
+        calculatorBrain?.pushOperand(4)
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperand(4)
+        calculatorBrain?.assertItEvaluatesTo(6.0)
     }
     
     func testBinaryOperationFollowByPreUnary(){
-        calculatorBrain!.pushOperand(1)
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperation("√")
-        calculatorBrain!.pushOperand(4)
-        XCTAssertEqual(3.0, calculatorBrain!.evaluate()!)
+        calculatorBrain?.pushOperand(1)
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperation("√")
+        calculatorBrain?.pushOperand(4)
+        calculatorBrain?.assertItEvaluatesTo(3.0)
     }
     
     func testPosUnaryFollowByBinaryOperation(){
-        calculatorBrain!.pushOperand(1)
-        calculatorBrain!.pushOperation("^2")
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperand(4)
-        XCTAssertEqual(5.0, calculatorBrain!.evaluate()!)
+        calculatorBrain?.pushOperand(1)
+        calculatorBrain?.pushOperation("^2")
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperand(4)
+        calculatorBrain?.assertItEvaluatesTo(5.0)
     }
     
     func testBinaryOperationFollowByPosUnary(){
-        calculatorBrain!.pushOperand(1)
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperand(4)
-        calculatorBrain!.pushOperation("^2")
-        XCTAssertEqual(17.0, calculatorBrain!.evaluate()!)
+        calculatorBrain?.pushOperand(1)
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperand(4)
+        calculatorBrain?.pushOperation("^2")
+        calculatorBrain?.assertItEvaluatesTo(17.0)
     }
     
     func testMultipleOperations(){
-        calculatorBrain!.pushOperand(1)
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperand(2)
-        calculatorBrain!.pushOperation("*")
-        calculatorBrain!.pushOperand(4)
-        calculatorBrain!.pushOperation("/")
-        calculatorBrain!.pushOperand(8)
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperand(5)
-        XCTAssertEqual(7.0, calculatorBrain!.evaluate()!)
+        calculatorBrain?.pushOperand(1)
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperand(2)
+        calculatorBrain?.pushOperation("*")
+        calculatorBrain?.pushOperand(4)
+        calculatorBrain?.pushOperation("/")
+        calculatorBrain?.pushOperand(8)
+        calculatorBrain?.pushOperation("-")
+        calculatorBrain?.pushOperand(1)
+        calculatorBrain?.assertItEvaluatesTo(1.0)
     }
 }
