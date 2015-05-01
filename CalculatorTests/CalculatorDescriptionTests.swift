703b9d3fa18958e878142cd74ba8306a6fc80dc6
tiagomartinho <tiago.martinho@xpeppers.com>
2015-05-01 18:22:19 +0200

    add 2nd calculator description test

--- CalculatorTests/CalculatorDescriptionTests.swift
+++ CalculatorTests/CalculatorDescriptionTests.swift
@@ -9,9 +9,15 @@ class CalculatorDescriptionTests: XCTestCase {
         calculatorBrain = CalculatorBrain()
     }
     
-    func testFunctionNotation() {
+    func testFunctionNotationForPreUnaryOperation() {
         calculatorBrain?.pushOperation("cos")
         calculatorBrain?.pushOperand(10.0)
         XCTAssertEqual("cos(10)", calculatorBrain?.description ?? "")
+    }
+    
+    func testFunctionNotationForPosUnaryOperation() {
+        calculatorBrain?.pushOperand(2)
+        calculatorBrain?.pushOperation("^2")
+        XCTAssertEqual("(2)^2", calculatorBrain?.description ?? "")
     }
 }
