d4ed40650cc5e45a55894bee658af33d5abd79a9
tiagomartinho <tiago.martinho@xpeppers.com>
2015-05-01 17:09:21 +0200

    refactor constants tests

--- CalculatorTests/CalculatorConstantsTests.swift
+++ CalculatorTests/CalculatorConstantsTests.swift
@@ -10,61 +10,56 @@ class CalculatorConstantsTests: XCTestCase {
     }
     
     func testOneConstant(){
-        calculatorBrain!.pushOperation("π")
-        XCTAssertEqualWithAccuracy(M_PI, calculatorBrain!.evaluate()!, 0.001)
-        
-        calculatorBrain!.clearOpStack()
-        
-        calculatorBrain!.pushOperation("e")
-        XCTAssertEqualWithAccuracy(M_E, calculatorBrain!.evaluate()!, 0.001)
+        calculatorBrain?.pushOperation("π")
+        calculatorBrain?.assertItEvaluatesTo(M_PI)
     }
     
 //    func testOneConstantWithUnaryOperation(){
-//        calculatorBrain!.pushOperation("π")
-//        calculatorBrain!.pushOperation("^2")
-//        XCTAssertEqualWithAccuracy(M_PI*M_PI, calculatorBrain!.evaluate()!, 0.001)
+//        calculatorBrain?.pushOperation("π")
+//        calculatorBrain?.pushOperation("^2")
+//        XCTAssertEqualWithAccuracy(M_PI*M_PI, calculatorBrain?.evaluate()?, 0.001)
 //    }
     
     func testTwoConstant(){
-        calculatorBrain!.pushOperation("e")
-        calculatorBrain!.pushOperation("π")
-        XCTAssertEqualWithAccuracy(M_E*M_PI, calculatorBrain!.evaluate()!, 0.001)
+        calculatorBrain?.pushOperation("e")
+        calculatorBrain?.pushOperation("π")
+        calculatorBrain?.assertItEvaluatesTo(M_E*M_PI)
     }
     
-    func testOneConstantWithOneOperand(){
-        calculatorBrain!.pushOperand(3)
-        calculatorBrain!.pushOperation("π")
-        XCTAssertEqualWithAccuracy(3*M_PI, calculatorBrain!.evaluate()!, 0.001)
-
-        calculatorBrain!.clearOpStack()
-
-        calculatorBrain!.pushOperation("e")
-        calculatorBrain!.pushOperand(5)
-        XCTAssertEqualWithAccuracy(M_E*5, calculatorBrain!.evaluate()!, 0.001)
+    func testOneConstantWithPreOperand(){
+        calculatorBrain?.pushOperand(3)
+        calculatorBrain?.pushOperation("π")
+        calculatorBrain?.assertItEvaluatesTo(3*M_PI)
+    }
+    
+    func testOneConstantWithPosOperand(){
+        calculatorBrain?.pushOperation("e")
+        calculatorBrain?.pushOperand(5)
+        calculatorBrain?.assertItEvaluatesTo(M_E*5)
     }
 
 //    func testOneConstantWithOneOperandAddConstant(){
-//        calculatorBrain!.pushOperand(3)
-//        calculatorBrain!.pushOperation("π")
-//        calculatorBrain!.pushOperation("+")
-//        calculatorBrain!.pushOperation("e")
-//        XCTAssertEqualWithAccuracy(3*M_PI+M_E, calculatorBrain!.evaluate()!, 0.001)
+//        calculatorBrain?.pushOperand(3)
+//        calculatorBrain?.pushOperation("π")
+//        calculatorBrain?.pushOperation("+")
+//        calculatorBrain?.pushOperation("e")
+//        XCTAssertEqualWithAccuracy(3*M_PI+M_E, calculatorBrain?.evaluate()?, 0.001)
 //    }
     
     func testOneConstantWithOneOperandAddOneOperandAndOneConstant(){
-        calculatorBrain!.pushOperand(3)
-        calculatorBrain!.pushOperation("π")
-        calculatorBrain!.pushOperation("+")
-        calculatorBrain!.pushOperand(5)
-        calculatorBrain!.pushOperation("e")
-        XCTAssertEqualWithAccuracy(3*M_PI+5*M_E, calculatorBrain!.evaluate()!, 0.001)
+        calculatorBrain?.pushOperand(3)
+        calculatorBrain?.pushOperation("π")
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperand(5)
+        calculatorBrain?.pushOperation("e")
+        calculatorBrain?.assertItEvaluatesTo(3*M_PI+5*M_E)
     }
     
     func testOneConstantWithOneOperandMultipliedByNumber(){
-        calculatorBrain!.pushOperand(3)
-        calculatorBrain!.pushOperation("π")
-        calculatorBrain!.pushOperation("*")
-        calculatorBrain!.pushOperand(5)
-        XCTAssertEqualWithAccuracy(3*M_PI*5, calculatorBrain!.evaluate()!, 0.001)
+        calculatorBrain?.pushOperand(3)
+        calculatorBrain?.pushOperation("π")
+        calculatorBrain?.pushOperation("*")
+        calculatorBrain?.pushOperand(5)
+        calculatorBrain?.assertItEvaluatesTo(3*M_PI*5)
     }
 }
