161337b233ab0ad2fd329a46c3693d8eddcb64e3
tiagomartinho <tiago.martinho@xpeppers.com>
2015-05-01 17:21:16 +0200

    add more tests to variable api

--- CalculatorTests/CalculatorAPITests.swift
+++ CalculatorTests/CalculatorAPITests.swift
@@ -26,10 +26,26 @@ class CalculatorAPITests: XCTestCase {
         calculatorBrain?.assertItEvaluatesTo(35.0)
     }
     
-    func testOneVariableWithUnaryOperation(){
-        calculatorBrain?.variableValues["x"] = 35.0
+    func testOneVariableWithPosUnaryOperation(){
         calculatorBrain?.pushOperand("x")
+        calculatorBrain?.variableValues["x"] = 35.0
         calculatorBrain?.pushOperation("^2")
         calculatorBrain?.assertItEvaluatesTo(1225.0)
+    }
+    
+    func testOneVariableWithPreUnaryOperation(){
+        calculatorBrain?.pushOperation("√")
+        calculatorBrain?.variableValues["x"] = 35.0
+        calculatorBrain?.pushOperand("x")
+        calculatorBrain?.assertItEvaluatesTo(5.916)
+    }
+    
+    func testOneOperandAddVariableWithOneOperand(){
+        calculatorBrain?.variableValues["M_E"] = M_E
+        calculatorBrain?.pushOperand(3)
+        calculatorBrain?.pushOperation("+")
+        calculatorBrain?.pushOperand(5)
+        calculatorBrain?.pushOperand("M_E")
+        calculatorBrain?.assertItEvaluatesTo(3+5*M_E)
     }
 }
